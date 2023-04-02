81b662076f21b076505e6028a4182e7026b5eda9
Jasper Wong <lazypigz.mw3@gmail.com>
2023-04-01 22:33:29 -0700

    Added comment for initial reading function

--- my-linux-code/include/accelerometer.h
+++ my-linux-code/include/accelerometer.h
@@ -13,6 +13,8 @@ void Accel_stop(void);
 
 // Saves a reading of the directional MSB values from the accelerometer to the passed in pointers.
 void Accel_getReading(unsigned char *dx, unsigned char *dy, unsigned char *dz);
+
+// Saves a reading of the initial directional MSB values from the accelerometer to the passed in pointers.
 void Accel_getInitialReading(unsigned char *dx, unsigned char *dy, unsigned char *dz);
 
 #endif
